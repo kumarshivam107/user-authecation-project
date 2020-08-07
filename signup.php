5063824216556c74ab105d0c36c99eeae7ba6928
kumarshivam107 <58832279+kumarshivam107@users.noreply.github.com>
2020-08-08 02:21:41 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -37,8 +37,8 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
                 $mail->isSMTP();                                            // Set mailer to use SMTP
                 $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
                 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                $mail->Username   = 'krshivam4545@gmail.com';                     // SMTP username
-                $mail->Password   = 'Assam@123';                               // SMTP password
+                $mail->Username   = 'Eamil Id';                     // SMTP username
+                $mail->Password   = 'Eamil Password';                               // SMTP password
                 $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
                 $mail->Port       = 587;                                    // TCP port to connect to
 
